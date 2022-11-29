d436800792e9fb34b3283621dcdf40935625ad64
Derek Truong <derektruongdev@gmail.com>
2022-11-29 16:32:15 -0600

    completed rps logic

--- Object Oriented Rock Paper Scissors/main.js
+++ Object Oriented Rock Paper Scissors/main.js
@@ -25,7 +25,7 @@ Rule
 
 - compare
 */
-const readline = required('readline-sync');
+const readline = require('readline-sync');
 
 const createPlayer = function(playerType) {
   return {
@@ -37,14 +37,14 @@ const createPlayer = function(playerType) {
       if (this.isHuman()) {
         console.log('Please enter a number to choose:\n(1) Rock\n(2) Paper\n(3) Scissors');
         let playerChoice = Number(readline.question().trim());
-        while (!playerChoice <= 3 || playerChoice >= 1) {
+        while (playerChoice > 3 || playerChoice < 1) {
           console.log('Please enter a valid number:\n(1) Rock\n(2) Paper\n(3) Scissors');
           playerChoice = Number(readline.question().trim());
         }
-        this.move = this.choices[playerChoice]
+        this.move = this.choices[playerChoice - 1]
       } else {
-        let randomIdx = Math.floor(Math.random * this.choices.length);
-        this.move = this.choices[randomIdx];
+          let randomIdx = Math.floor(Math.random() * this.choices.length);
+          this.move = this.choices[randomIdx];
       }
     },
 
@@ -83,11 +83,33 @@ const RPSGame = {
     console.log('Thanks for playing. Goodbye!');
   },
 
+  displayWinner() {
+    const WINNING_CONDITIONS = {
+      rock: 'scissors',
+      paper: 'rock',
+      scissors: 'paper'
+    }
+    const win = WINNING_CONDITIONS[this.human.move] === this.computer.move;
+    const lose = WINNING_CONDITIONS[this.human.move] !== this.computer.move;
+    const draw = this.human.move === this.computer.move;
+
+    console.log(`You chose: ${this.human.move}`);
+    console.log(`The computer chose: ${this.computer.move}`);
+
+    if (win) {
+      console.log('You Win!');
+    } else if (draw) {
+      console.log('It\s a tie.');
+    } else {
+      console.log('You Lose.')
+    }
+  },
+
   play() {
     this.displayWelcomeMessage();
     this.human.choose();
     this.computer.choose();
-    displayWinner();
+    this.displayWinner();
     this.displayGoodbyeMessage();
   }
 };
