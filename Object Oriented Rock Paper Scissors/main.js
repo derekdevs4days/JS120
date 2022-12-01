0122a27a54e20f556747ce696b8023e84d7ebe28
Derek Truong <derektruongdev@gmail.com>
2022-12-01 16:35:25 -0600

    updated files

--- Object Oriented Rock Paper Scissors/main.js
+++ Object Oriented Rock Paper Scissors/main.js
@@ -1,79 +1,42 @@
 /*
-*** STEP 1 ***
-Write a textual description of the problem.
-
-RPS is a two-player game where each player chooses one of three possible moves: rock, paper, or scissors. The winner is chosen by comparing their moves with the following rules:
-
-- ROCK beats SCISSORS
-- SCISSORS beats PAPER
-- PAPER beats ROCK
-- tie if same choice'
-
-*** STEP 2 ***
-Extract the significant nouns and verbs from description.
-
-Nouns: player, move, rule.
-Verbs: choose, compare
-
-*** STEP 3 ***
-Organize and associate the verbs with the nouns.
- 
-Player
- - choose
-Move 
-Rule
-
-- compare
+TODO
+- best out of 5 games wins.
 */
 const readline = require('readline-sync');
 
-const createPlayer = function(playerType) {
+const createPlayer = function() {
   return {
-    playerType: playerType,
-    choices: ['rock', 'paper', 'scissors'],
     move: null,
 
     choose() {
-      if (this.isHuman()) {
-        console.log('Please enter a number to choose:\n(1) Rock\n(2) Paper\n(3) Scissors');
-        let playerChoice = Number(readline.question().trim());
-        while (playerChoice > 3 || playerChoice < 1) {
-          console.log('Please enter a valid number:\n(1) Rock\n(2) Paper\n(3) Scissors');
-          playerChoice = Number(readline.question().trim());
-        }
-        this.move = this.choices[playerChoice - 1]
-      } else {
-          let randomIdx = Math.floor(Math.random() * this.choices.length);
-          this.move = this.choices[randomIdx];
+      const options = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+      console.log('Please enter a number to choose:\n(1) Rock\n(2) Paper\n(3) Scissors\n(4) Lizard\n(5) Spock');
+      let choice = Number(readline.question().trim());
+      while (Number.isNaN(choice) || choice > 5 || choice < 1) {
+        console.log('Please enter a valid number:\n(1) Rock\n(2) Paper\n(3) Scissors\n(4) Lizard\n(5) Spock');
+        choice = Number(readline.question().trim());
       }
+      this.move = options[choice - 1];
     },
-
-    isHuman() {
-      return this.playerType === 'human';
-    },
-
   };
-}
+};
 
-const createMove = function() {
+const createComputer = function() {
   return {
-    // possible state: type of move (rock, paper, scissors)
+    move: null,
+
+    choose() {
+      const options = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+      let randomIdx = Math.floor(Math.random() * options.length);
+      this.move = options[randomIdx];
+    }
   };
-}
-
-const createRule = function() {
-  return {
-    // rules?
-  };
-}
-
-const compare = function(move1, move2) {
-
 };
 
 const RPSGame = {
-  human: createPlayer('human'),
-  computer: createPlayer('computer'),
+  human: createPlayer(),
+  computer: createComputer(),
+  score: {round: 1, humanScore: 0, computerScore: 0},
 
   displayWelcomeMessage() {
     console.log('Welcome to Rock, Paper, Scissors!');
@@ -84,32 +47,50 @@ const RPSGame = {
   },
 
   displayWinner() {
-    const WINNING_CONDITIONS = {
-      rock: 'scissors',
-      paper: 'rock',
-      scissors: 'paper'
-    }
-    const win = WINNING_CONDITIONS[this.human.move] === this.computer.move;
-    const lose = WINNING_CONDITIONS[this.human.move] !== this.computer.move;
+    const WINNING = { rock: {scissors: 1, lizard: 1},
+      paper: {rock: 1, spock: 1}, scissors: {paper: 1, lizard: 1},
+      lizard: {paper: 1, spock: 1}, spock: {scissors: 1, rock: 1},
+    };
+    const win = WINNING[this.human.move].hasOwnProperty(this.computer.move);
     const draw = this.human.move === this.computer.move;
-
     console.log(`You chose: ${this.human.move}`);
     console.log(`The computer chose: ${this.computer.move}`);
-
     if (win) {
+      this.score.humanScore += 1;
       console.log('You Win!');
     } else if (draw) {
-      console.log('It\s a tie.');
+      console.log('It\'s a tie.');
     } else {
-      console.log('You Lose.')
+      this.score.computerScore += 1;
+      console.log('You Lose.');
     }
+    this.score.round += 1;
+  },
+
+  displayScore() {
+    console.log(`=> This is round ${this.score.round}`);
+    console.log(`=> Player Score: ${this.score.humanScore}  Computer Score: ${this.score.computerScore}`);
+  },
+
+  playAgain() {
+    console.log('Do you want to play again? ( Y / N )');
+    let playMore = readline.question().toLowerCase();
+    while (playMore !== 'y' && playMore !== 'n') {
+      console.log('Please enter a valid choice. ( Y / N )');
+      playMore = readline.question().toLowerCase();
+    }
+    return playMore === 'y';
   },
 
   play() {
     this.displayWelcomeMessage();
-    this.human.choose();
-    this.computer.choose();
-    this.displayWinner();
+    while (true) {
+      this.human.choose();
+      this.computer.choose();
+      this.displayWinner();
+      this.displayScore();
+      if (!this.playAgain()) break;
+    }
     this.displayGoodbyeMessage();
   }
 };
